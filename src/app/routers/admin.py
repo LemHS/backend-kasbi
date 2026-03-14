6f73b751b86129c19463bfc39e6d3ac9523edbc5
horas <lemuelhorasdaniel@gmail.com>
2026-03-14 11:53:23 +0700

    fixed dashboard #2

--- src/app/routers/admin.py
+++ src/app/routers/admin.py
@@ -306,4 +306,4 @@ def dashboard(
     return APIResponse(
         status_code=200, 
         message="Data returned successfully", 
-        data=DashboardResponse(user_counts=dict(user_counts), chat_counts=dict(chat_counts)))
+        data=DashboardResponse(user_counts=tuple(user_counts._mapping), chat_counts=tuple(chat_counts._mapping)))
